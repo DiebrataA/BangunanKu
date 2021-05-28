756a9125b7de7d39879b354cd9f857c1b68cbe06
Anggara Diebrata <angga.deebrata@gmail.com>
2021-05-28 20:07:30 +0700

    Initial commit

---
+++ app/src/main/java/com/anggarad/dev/bangunganku/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.anggarad.dev.bangunganku.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anggarad.dev.bangunganku.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/anggarad/dev/bangunganku/ui/service/building/BuildingReportActivity.kt
@@ -0,0 +1,12 @@
+package com.anggarad.dev.bangunganku.ui.service.building
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anggarad.dev.bangunganku.R
+
+class BuildingReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_building_report)
+    }
+}
